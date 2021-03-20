8a4907be02d434ef14c8bea4f54e5158ba0c8400
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2021-03-20 22:17:06 +0100

    Add files via upload

---
+++ Riconoscitore vocale Python(linguaITA)/Ricono-vocaITA.py
@@ -0,0 +1,27 @@
+import speech_recognition as sr
+import keyboard
+import sys
+
+def inizio():
+    try:
+        Riconoscitorevoce = sr.Recognizer()
+        print("Lingua ITALIANO")
+        print("In ascolto")
+        with sr.Microphone() as mic:
+            Riconoscitorevoce.adjust_for_ambient_noise(mic)
+            
+            audio = Riconoscitorevoce.listen(mic)
+            print("Ok! sto ora elaborando il messaggio!")
+            
+            text = Riconoscitorevoce.recognize_google(audio, language="it-IT")
+            
+            print(text)
+                f = open('Scrittore.txt', 'a')
+                f.write(text)
+                f.write('\n')
+                f.close()            
+    except:
+        print("Errore, non ho rilevato una voce, o è poco chiara o non è inglese")
+inizio()
+    
+    
